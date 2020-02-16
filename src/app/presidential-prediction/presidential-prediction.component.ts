40d617ac2d5ddf5815f34e470ffb1b6ee2e282ed
will allen <william.allen1296@gmail.com>
2020-02-15 18:55:44 -0600

    added basic content to pres prediction

--- src/app/presidential-prediction/presidential-prediction.component.ts
+++ src/app/presidential-prediction/presidential-prediction.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-presidential-prediction',
@@ -7,9 +8,120 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PresidentialPredictionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
 
+
+  //Can this be made into a function that takes a form group so it is reusable?  
+  checkOther() {
+    this.predictionForm.controls.candidate1Info.valueChanges.subscribe(value => {
+      console.log(value.candidate1);
+
+      if (value.candidate1 == 'Other:') {
+        this.showOther = true;
+        //this.predictionForm.controls.candidate1Info.value.candidate1 = this.predictionForm.controls.candidate1Info.value.c1Other;
+        let x = this.predictionForm.controls.candidate1Info.value.c1Other;
+        console.log(x); //can we now pass x as this value instead? if c1Other is not null pass it instead?
+        //console.log(this.predictionForm.controls.candidate1Info.value.candidate1);
+      } else {
+        this.showOther = false;
+      }
+
+    })
+  }
+
+  cans: string[] = ['Donald Trump', 'Mike Pence', 'Other:'];
+  showOther: boolean = false;
+  canOneParties: string[] = ['Republican', 'Democrat', 'Independent', 'Other'];
+  canOneVPs: string[] = ['Mike Pence', 'Other'];
+
+  canTwos: string[] = ['Bernie', 'Buhda Judge', 'Liz Warren'];
+  canTwoParties: string[] = ['Democrat', 'Independent', 'Coomunist', 'Tea Party', 'Anarchist', 'Polar Regions'];
+  canTwoVPs: string[] = ['Steve Martin', 'Beto', 'AOC'];
+
+  popularVoteWinners: string[] = ['Steve', 'Erwin'];
+  electoralVoteWinners: string[] = ['Jooee','Smiiiith']
+  electionWinners: string[] = ['Jose', 'Al', 'Breg', 'inger'];
+
+  parties: string[] = ['Democrat', 'Independent', 'Coomunist', 'Tea Party', 'Anarchist', 'Polar Regions'];
+
+
+  predictionForm = this.fb.group({
+    candidate1Info: this.fb.group({
+      candidate1: [''],
+      c1Other: [''],
+      candidate1Party: new FormControl({value: 'Republican', disabled: true}),
+      candidate1VP: [''],
+      candidate1Faithless: [''],
+    }),
+    candidate2Info: this.fb.group({
+      candidate2: [''],
+      candidate2Party: [''],
+      candidate2VP: [''],
+      candidate2Faithless: [''],
+    }),
+    generalInfo: this.fb.group({
+      popularVoteWinner: [''],
+      electoralVoteWinner: [''],
+      electionWinner: [''],
+      description: [''],
+      why: [''],
+    }),
+    stateInfo: this.fb.group({
+      ALVote: [''],
+      AKVote: [''],
+      AZVote: [''],
+      ARVote: [''],
+      CAVote: [''],
+      COVote: [''],
+      CTVote: [''],
+      DEVote: [''],
+      GAVote: [''],
+      HIVote: [''],
+      IDVote: [''],
+      ILVote: [''],
+      INVote: [''],
+      KSVote: [''],
+      KYVote: [''],
+      LAVote: [''],
+      MEVote: [''],
+      MDVote: [''],
+      MAVote: [''],
+      MIVote: [''],
+      MNVote: [''],
+      MSVote: [''],
+      MOVote: [''],
+      MTVote: [''],
+      NEVote: [''],
+      NVVote: [''],
+      NJVote: [''],
+      NMVote: [''],
+      NYVote: [''],
+      NCVote: [''],
+      NDVote: [''],
+      OKVote: [''],
+      ORVote: [''],
+      RIVote: [''],
+      SCVote: [''],
+      SDVote: [''],
+      TNVote: [''],
+      TXVote: [''],
+      UTVote: [''],
+      VTVote: [''],
+      WAVote: [''],
+      WVVote: [''],
+      WYVote: ['']
+    }),
+    swingStateInfo: this.fb.group({
+      OHVote: [''],
+      WIVote: [''],
+      VAVote: [''],
+      PAVote: [''],
+      NHVote: [''],
+      IAVote: [''],
+      FLVote: [''],
+    })
+  });
 }
