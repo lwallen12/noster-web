b2a1b10e60b1e01cffbcbc46149c2ce6a330f7eb
will allen <william.allen1296@gmail.com>
2020-02-18 18:37:21 -0600

    Have basic Pres Pred complete

--- src/app/presidential-prediction/presidential-prediction.component.ts
+++ src/app/presidential-prediction/presidential-prediction.component.ts
@@ -15,46 +15,51 @@ export class PresidentialPredictionComponent implements OnInit {
 
 
   //Can this be made into a function that takes a form group so it is reusable?  
-  checkOther() {
-    this.predictionForm.controls.candidate1Info.valueChanges.subscribe(value => {
-      console.log(value.candidate1);
+  // checkOther() {
+  //   this.predictionForm.controls.candidate1Info.valueChanges.subscribe(value => {
+  //     console.log(value.candidate1);
 
-      if (value.candidate1 == 'Other:') {
-        this.showOther = true;
-        //this.predictionForm.controls.candidate1Info.value.candidate1 = this.predictionForm.controls.candidate1Info.value.c1Other;
-        let x = this.predictionForm.controls.candidate1Info.value.c1Other;
-        console.log(x); //can we now pass x as this value instead? if c1Other is not null pass it instead?
-        //console.log(this.predictionForm.controls.candidate1Info.value.candidate1);
-      } else {
-        this.showOther = false;
-      }
+  //     if (value.candidate1 == 'Other:') {
+  //       this.showOther = true;
+  //       //this.predictionForm.controls.candidate1Info.value.candidate1 = this.predictionForm.controls.candidate1Info.value.c1Other;
+  //       let x = this.predictionForm.controls.candidate1Info.value.c1Other;
+  //       console.log(x); //can we now pass x as this value instead? if c1Other is not null pass it instead?
+  //       //console.log(this.predictionForm.controls.candidate1Info.value.candidate1);
+  //     } else {
+  //       this.showOther = false;
+  //     }
 
-    })
-  }
+  //   })
+  // }
 
-  cans: string[] = ['Donald Trump', 'Mike Pence', 'Other:'];
-  showOther: boolean = false;
+  cans: string[] = ['Donald Trump', 'Mike Pence', 'Zorp the Surveyor', 'Other'];
   canOneParties: string[] = ['Republican', 'Democrat', 'Independent', 'Other'];
-  canOneVPs: string[] = ['Mike Pence', 'Other'];
+  canOneVPs: string[] = ['Mike Pence', 'Other', 'Herb Scaifer'];
 
   canTwos: string[] = ['Bernie', 'Buhda Judge', 'Liz Warren'];
-  canTwoParties: string[] = ['Democrat', 'Independent', 'Coomunist', 'Tea Party', 'Anarchist', 'Polar Regions'];
+  canTwoParties: string[] = ['Democrat', 'Independent', 'Coomunist', 'Tea Party', 'Anarchist', 'Polar Regions', 'Republican'];
   canTwoVPs: string[] = ['Steve Martin', 'Beto', 'AOC'];
 
-  popularVoteWinners: string[] = ['Steve', 'Erwin'];
-  electoralVoteWinners: string[] = ['Jooee','Smiiiith']
-  electionWinners: string[] = ['Jose', 'Al', 'Breg', 'inger'];
 
-  parties: string[] = ['Democrat', 'Independent', 'Coomunist', 'Tea Party', 'Anarchist', 'Polar Regions'];
+  electionWinners: string[] = ['Donald Trump','Pete Buttigieg','Bernie Sanders','Elizabeth Warren','Amy Klobuchar',
+  'Joe Biden','Andrew Yang','Michael Bloomberg','Kamala Harris','Hillary Clinton', 'Mike Pence', 'Herb Scaifer', 'Zeke Schrute', 'Zorp the Surveyor '];
+
+  parties: string[] = ['Democrat', 'Independent', 'Coomunist', 'Tea Party', 'Anarchist', 'Polar Regions', 'Republican'];
+
+
+  allParties: string[] = ['Democrat', 'Republican', 'Independent or Minor Party', 'The Reasonabilists']
+  opCandidates: string[] = ['Pete Buttigieg','Bernie Sanders','Elizabeth Warren','Amy Klobuchar',
+    'Joe Biden','Andrew Yang','Michael Bloomberg','Kamala Harris','Hillary Clinton', 'Zeke Schrute']
+
+
 
 
   predictionForm = this.fb.group({
     candidate1Info: this.fb.group({
-      candidate1: [''],
-      c1Other: [''],
+      candidate1: new FormControl({value: 'Donald Trump', disabled: false}),
       candidate1Party: new FormControl({value: 'Republican', disabled: true}),
-      candidate1VP: [''],
-      candidate1Faithless: [''],
+      candidate1VP: new FormControl({value: 'Mike Pence', disabled: false}),
+      candidate1Faithless: new FormControl({value: 0}),
     }),
     candidate2Info: this.fb.group({
       candidate2: [''],
@@ -115,7 +120,7 @@ export class PresidentialPredictionComponent implements OnInit {
       WYVote: ['']
     }),
     swingStateInfo: this.fb.group({
-      OHVote: [''],
+      OHVote: new FormControl({value: 'Republican', disabled: false}),
       WIVote: [''],
       VAVote: [''],
       PAVote: [''],
